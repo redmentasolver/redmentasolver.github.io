c28e2ad1425e18f44ecb1159c79586c090375e1f
redmentasolver <156482166+redmentasolver@users.noreply.github.com>
2024-01-24 21:47:33 +0100

    Update activateCode.js

--- src/activateCode.js
+++ src/activateCode.js
@@ -25,7 +25,7 @@ async function main() {
             "content-type": 'application/json',
         }
     })
-    const sha = sha256(code.toString() + api.git)
+    const sha = sha256(code.toString() + api.git + time)
     let found = false
     let response;
     console.log("Megpróbáljuk a kódot... Ez eltarthat egy pár percig")
