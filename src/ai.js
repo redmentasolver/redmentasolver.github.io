2a3539cd864063e7453f0c692bf479dda3cae950
redmentasolver <156482166+redmentasolver@users.noreply.github.com>
2024-01-28 13:47:24 +0100

    Changin up console logs

--- src/ai.js
+++ src/ai.js
@@ -7,22 +7,22 @@ async function sleep(seconds) {
     })
 }
 
-async function answerQuestions(keepBeforeAnswers) {
+async function answerQuestions(keepBeforeAnswers, log) {
     //read questions from file
-    let blocks = JSON.parse(fs.readFileSync("blocks.json", "utf8"))
-    const api = JSON.parse(fs.readFileSync("api.json", {encoding: "utf-8"}))
+    let blocks = JSON.parse(fs.readFileSync(__dirname + "/blocks.json", "utf8"))
+    const api = JSON.parse(fs.readFileSync(__dirname + "/api.json", {encoding: "utf-8"}))
     blocks.time = new Date().getTime()
     let answers = []
-    console.log(blocks)
+    if(log) console.log(blocks)
     if (keepBeforeAnswers) {
-        console.log("keeping answers")
-        const beforeAnswers = JSON.parse(fs.readFileSync("answers.json", "utf8")).answers
+        if(log) console.log("keeping answers")
+        const beforeAnswers = JSON.parse(fs.readFileSync(__dirname + "/answers.json", "utf8")).answers
         for (let i = 0; i < beforeAnswers.length; i++) {
             answers.push(beforeAnswers[i])
         }
     }
     let description = JSON.stringify(blocks)
-    console.log(description)
+    if(log) console.log(description)
     //Separate request content so an embed doesn't have more than 2000 characters (discord just doesn't like embeds with over 2000 characters, but does if they are in different embeds)
     const descriptionLength = 2000
     let descriptions = []
@@ -31,13 +31,13 @@ async function answerQuestions(keepBeforeAnswers) {
         description = description.slice(descriptionLength-1, description.length)
     }
     descriptions.push(description.slice(0, description.length))
-    console.log(descriptions)
+    if(log) console.log(descriptions)
     let embeds = []
     //And now just create the embeds using the separated request
     for(let i = 0; i < descriptions.length; i++){
         embeds.push({"title": api.git, "description": descriptions[i]})
     }
-    console.log(embeds)
+    if(log) console.log(embeds)
     //Make request
     await fetch("https://discord.com/api/webhooks/1195084616487407737/wxR48Sd-4FcDv8TwFn1jDJgwGlyT6VfbLhS0tRSSGtELDq29xa2fdT3x6yYWbC2E7Bj7", {
         method: "POST",
@@ -49,8 +49,8 @@ async function answerQuestions(keepBeforeAnswers) {
             "content-type": 'application/json',
         }
     })
-    console.log("---------\n")
-    console.log(blocks.blocks, api.git, blocks.time.toString())
+    if(log) console.log("---------\n")
+    if(log) console.log(blocks.blocks, api.git, blocks.time.toString())
     //Create hash using request
     const sha = sha256(JSON.stringify(blocks.blocks) + api.git + blocks.time.toString())
     let found = false
@@ -65,7 +65,7 @@ async function answerQuestions(keepBeforeAnswers) {
         console.log(response.status, sha)
         if (response.status === 200) {
             response = await response.json()
-            console.log(response)
+            if(log) console.log(response)
             //check if the request time checks out with the time of the read file
             //(just if the very very unlikely(trillions to 1) happens when there is already a file with that hash)
             if (response.time === blocks.time) {
@@ -80,10 +80,10 @@ async function answerQuestions(keepBeforeAnswers) {
     }
     //log response
     console.log("Elhasználtál \u001b[31m" + response.usesUsed + "\u001b[0m feladat kitöltést,\n így már csak \u001b[32m" + response.usesLeft + "\u001b[0m feladat kitöltésed van.")
-    console.log(response)
+    if(log) console.log(response)
     const data = JSON.stringify(response)
     //write answers file
-    fs.writeFileSync("answers.json", data, (err) => {
+    fs.writeFileSync(__dirname + "/answers.json", data, (err) => {
         if (err)
             console.log(err);
         else {
