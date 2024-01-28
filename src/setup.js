dcd0e6758533ddfb78636ee16adf33497a7d4695
redmentasolver <156482166+redmentasolver@users.noreply.github.com>
2024-01-28 14:03:11 +0100

    Adding looking for key

--- src/setup.js
+++ src/setup.js
@@ -4,8 +4,18 @@ async function setup() {
     const prompt = require("prompt-sync")();
     const fs = require("fs")
     const update = require("./update")
-    const key = prompt("A (titkos) kulcsod: ")
-    fs.writeFileSync("api.json", JSON.stringify({"git": key, "version": (await update.getVersionDetails()).version}))
+    let key = null
+    try{
+        const api = JSON.parse(fs.readFileSync(__dirname + "/api.json"))
+        if(api.git == null) throw new Error()
+        console.log("Megtaláltuk ezt a kulcsot a fájlok között: '\u001b[32m" + api.git + "\u001b[0m' Szeretnéd ezt használni?")
+        const choice = prompt("ENTER ha igen CTRL+C ha nem")
+        if(choice == null) throw new Error()
+        else key = api.git
+    } catch(err){
+        key = prompt("A (titkos) kulcsod: ")
+    }
+    fs.writeFileSync(__dirname + "/api.json", JSON.stringify({"git": key, "version": (await update.getVersionDetails()).version}))
     fs.writeFileSync("ACTIVATECODE.bat", "node activateCode.js")
 }
 module.exports = {setup}
